c24fbe6b2024fff2c42a1a0be04b4704b3a42545
Eric <xplzv@126.com>
2026-09-03 17:04:36 +0800

    perf: remove code

--- pkg/utils/math.go
+++
@@ -1,6 +0,0 @@
-package utils
-
-func GetMaxInt() int {
-	maxInt := int(^uint(0) >> 1)
-	return maxInt
-}
--- ui/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createPinia } from 'pinia'
-
-export const store = createPinia()
-
-export function setupStore(app) {
-  app.use(store)
-}
-
-export * from './modules'
